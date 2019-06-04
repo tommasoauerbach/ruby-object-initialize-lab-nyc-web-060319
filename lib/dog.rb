38f478443f3e2f5ac511bccff42595e0f7156903
tommaso auerbach <tsa302@nyu.edu>
2019-06-04 20:41:11 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  end
